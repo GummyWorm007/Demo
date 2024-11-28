6acc3fcdc5f0bd8fe5f924235c689599bb9c5eb5
GummyWorm007 <timothy.rix007@gmail.com>
2024-11-28 22:54:33 +0200

    Running
    I fine tuned the player speed and running animations

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,8 +4,13 @@ extends CharacterBody3D
 @onready var animation_player: AnimationPlayer = $visuals/mixamo_base/AnimationPlayer
 @onready var visuals: Node3D = $visuals
 
-const SPEED = 2.9
+var SPEED = 2.9
 const JUMP_VELOCITY = 4.5
+
+var walking_speed = 2.9
+var running_speed = 5
+
+var running = false
 
 @export var sens_horizontal = 0.5
 @export var sens_vertical = 0.5
@@ -20,6 +25,16 @@ func _input(event):
 		camera_mount.rotate_x(deg_to_rad(-event.relative.y*sens_vertical))
 
 func _physics_process(delta: float) -> void:
+	
+	if Input.is_action_pressed("run"):
+		SPEED = running_speed
+		running = true
+	else:
+		SPEED = walking_speed
+		running = false 
+	
+	
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -33,8 +48,12 @@ func _physics_process(delta: float) -> void:
 	var input_dir := Input.get_vector("left", "right", "forward", "backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		if animation_player.current_animation != "walking":
-			animation_player.play("walking")
+		if running:
+			if animation_player.current_animation != "running":
+				animation_player.play("running")
+		else: 
+			if animation_player.current_animation != "walking":
+				animation_player.play("walking")
 		
 		visuals.look_at(position + direction)
 		
